fb9ad32a25926d096632884605f6a28c8205304c
AdamKvant <adamkvant@gmail.com>
2024-09-29 01:49:16 -0500

    Some intitial setup and roughhhh homepage

---
+++ CulinaryCards/__init__.py
@@ -0,0 +1,15 @@
+import os
+import psycopg2
+from datetime import *
+from flask import Flask
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/search",methods=['POST'])
+def redirectToSearch():
+    return render_template("test.html")
---
+++ schema.sql
@@ -0,0 +1,1 @@
+Insert DB info here
