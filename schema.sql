a9556180cc064082ac1cde23ced8dbeb654a04d4
AdamKvant <adamkvant@gmail.com>
2024-10-02 21:27:30 -0500

    db updated

--- schema.sql
+++ schema.sql
@@ -1,26 +1,32 @@
-CREATE TABLE user(
+create table users (
     user_email varchar(255) primary key,
-    Fname varchar(40),
-    Lname varchar(60),
+    fname varchar(40),
+    lname varchar(60),
     bio varchar(300)
 );
 
-CREATE TABLE recipe(
+create table recipe (
     recipe_id serial primary key,
-    description varchar(255);
+    description varchar(255),
     ingredients text[] not null,
     instructions text[] not null,
-    created_on timestamp default NOW(),
+    created_on timestamp default now(),
+    user_email varchar(255),
     constraint fk_user
         foreign key (user_email)
-        references user (user_email)
+        references users (user_email)
         on delete cascade
 );
 
-create table recipe_like(
-    likeID serial primary key,
-    like_time timestamp default NOW(),
-    constraint fk_like
-    foreign key (recipe_id) references recipe(recipe_id)
-    foreign key (user_email) references user(user_email)
+create table recipe_like (
+    likeid serial primary key,
+    like_time timestamp default now(),
+    recipe_id int,
+    user_email varchar(255),
+    constraint fk_recipe
+        foreign key (recipe_id)
+        references recipe (recipe_id),
+    constraint fk_user
+        foreign key (user_email)
+        references users (user_email)
 );
