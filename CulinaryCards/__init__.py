bf776e6932a088981f1a33db6f8754f5e1c18100
root <root@AnimeMachine>
2024-10-03 23:16:24 -0500

    Basic Profile Page

--- CulinaryCards/__init__.py
+++ CulinaryCards/__init__.py
@@ -10,12 +10,22 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/createRecipe")
+def createRecipe():
+    return render_template("createRecipe.html")
+
 @app.route("/search",methods=['POST'])
 def redirectToSearch():
     return render_template("test.html")
 
 if __name__ == "__main__":
     if os.getenv("FLASK_ENV") == "development":
+        app.config['TEMPLATES_AUTO_RELOAD'] = True
+        app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
         app.run(debug=True)
     else:
         app.run()
