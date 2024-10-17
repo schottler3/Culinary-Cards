084202d6f4c016d809894342316342a3daae2d01
Adam Kvant <kvant003@umn.edu>
2024-10-17 12:19:15 -0500

    fixed recipe of the day

--- recipeOfTheDay.py
+++ recipeOfTheDay.py
@@ -7,15 +7,16 @@ def setRecipeOfTheDay():
     global recipeOfTheDay
     all_recipes = db.getAllRecipes()
     all_recipeslen = len(all_recipes)
-    randindex = datetime.now().timetuple().tm_mday % all_recipeslen
-    recipeOfTheDay = all_recipes[randindex]
-    ingredientstr = ""
-    for ingredient in recipeOfTheDay[3]:
-        ingredientstr += ingredient.split(",")[0] + ", "
-    ingredientstr = ingredientstr[:-2:]
-    recipeOfTheDay = list(recipeOfTheDay)
-    recipeOfTheDay[3] = ingredientstr
-    recipeOfTheDay = tuple(recipeOfTheDay)
+    if all_recipeslen > 0:
+        randindex = datetime.now().timetuple().tm_mday % all_recipeslen
+        recipeOfTheDay = all_recipes[randindex]
+        ingredientstr = ""
+        for ingredient in recipeOfTheDay[3]:
+            ingredientstr += ingredient.split(",")[0] + ", "
+        ingredientstr = ingredientstr[:-2:]
+        recipeOfTheDay = list(recipeOfTheDay)
+        recipeOfTheDay[3] = ingredientstr
+        recipeOfTheDay = tuple(recipeOfTheDay)
 
 def getRecipeOfTheDay():
     return recipeOfTheDay
